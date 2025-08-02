6850eb1aed689d8e09ac2faca92a59d2c2727873
Vanya Holovey <golovejvana9@gmail.com>
2025-08-02 14:04:27 +0300

    react keyboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+export const App: React.FC = () => {
+  const [pressedKey, setPressedKey] = useState('');
+
+  useEffect(() => {
+    const handlePressKey = (event: KeyboardEvent) => {
+      setPressedKey(event.key);
+    };
+
+    document.addEventListener('keyup', handlePressKey);
+
+    return () => {
+      document.removeEventListener('keyup', handlePressKey);
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <p className="App__message">
+        {pressedKey === ''
+          ? 'Nothing was pressed yet'
+          : `The last pressed key is [${pressedKey}]`}
+      </p>
+    </div>
+  );
+};
